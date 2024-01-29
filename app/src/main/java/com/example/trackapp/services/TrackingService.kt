733a5bf189420a3b49b0ec719c70bd97040d5d15
GergesElbana <nahla@el-abda3.net>
2024-01-29 07:30:00 -0800

    Drawing the tracking on  the map

--- app/src/main/java/com/example/trackapp/services/TrackingService.kt
+++ app/src/main/java/com/example/trackapp/services/TrackingService.kt
@@ -1,44 +1,91 @@
 package com.example.trackapp.services
 
+import android.annotation.SuppressLint
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.NotificationManager.IMPORTANCE_LOW
 import android.app.PendingIntent
+import android.app.PendingIntent.FLAG_MUTABLE
 import android.app.PendingIntent.FLAG_UPDATE_CURRENT
 import android.content.Context
 import android.content.Intent
+import android.location.Location
+import android.location.LocationRequest
 import android.os.Build
+import android.os.Looper
 import androidx.annotation.RequiresApi
 import androidx.core.app.NotificationCompat
 import androidx.lifecycle.LifecycleService
+import androidx.lifecycle.MediatorLiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Observer
 import com.example.trackapp.R
 import com.example.trackapp.other.Constants.ACTION_PAUSE_SERVICE
 import com.example.trackapp.other.Constants.ACTION_SHOW_TRACKING_FRAGMENT
 import com.example.trackapp.other.Constants.ACTION_START_OR_RESUME_SERVICE
 import com.example.trackapp.other.Constants.ACTION_STOP_SERVICE
+import com.example.trackapp.other.Constants.FASTEST_LOCATION_INTERVAL
+import com.example.trackapp.other.Constants.LOCATION_UPDATE_INTERVAL
 import com.example.trackapp.other.Constants.NOTIFICATION_CHANNEL_ID
 import com.example.trackapp.other.Constants.NOTIFICATION_CHANNEL_NAME
 import com.example.trackapp.other.Constants.NOTIFICATION_ID
+import com.example.trackapp.other.TrackingUtility
 import com.example.trackapp.ui.MainActivity
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationResult
+import com.google.android.gms.location.LocationRequest.PRIORITY_HIGH_ACCURACY
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.maps.model.LatLng
 import timber.log.Timber
+
+typealias Polyline = MutableList<LatLng>
+typealias Polylines = MutableList<Polyline>
 
 class TrackingService : LifecycleService() {
 
-    var isFirstRun=true
+    var isFirstRun = true
+    lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+
+    private val timeRunSeconds = MutableLiveData<Long>()
+
+    companion object {
+        val isTracking = MutableLiveData<Boolean>()
+        val pathPoints = MutableLiveData<Polylines>()
+    }
+
+    fun postInitialValue() {
+        val timeRunInMilliSecond = MutableLiveData<Long>()
+        isTracking.postValue(false)
+        pathPoints.postValue(mutableListOf())
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        postInitialValue()
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
+
+        isTracking.observe(this, Observer {
+            updateLocationTracking(it)
+        })
+    }
+
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
         intent?.let {
             when (it.action) {
                 ACTION_START_OR_RESUME_SERVICE -> {
-                    if (isFirstRun){
+                    if (isFirstRun) {
                         startForegroundService()
-                        isFirstRun=false
-                    }else{
+                        isFirstRun = false
+                    } else {
                         Timber.d("Resuming service...")
+                        startForegroundService()
                     }
                 }
 
                 ACTION_PAUSE_SERVICE -> {
                     Timber.d("Pause service")
+                    pauseService()
                 }
 
                 ACTION_STOP_SERVICE -> {
@@ -49,13 +96,79 @@ class TrackingService : LifecycleService() {
         return super.onStartCommand(intent, flags, startId)
     }
 
+    private val isTimerEnabled = false
+    private val lapTime = 0L
+    private val timeRun = 0L
+    private val timeStarted = 0L
+    private val lastSecondTimestamp = 0L
+
+    private fun startTimer(){
+
+    }
+    private fun pauseService() {
+        isTracking.postValue(false)
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun updateLocationTracking(isTracking: Boolean) {
+        if (isTracking) {
+            if (TrackingUtility.hasLocationPermission(this)) {
+                val request = com.google.android.gms.location.LocationRequest().apply {
+                    interval = LOCATION_UPDATE_INTERVAL
+                    fastestInterval = FASTEST_LOCATION_INTERVAL
+                    priority = PRIORITY_HIGH_ACCURACY
+                }
+
+                fusedLocationProviderClient.requestLocationUpdates(
+                    request,
+                    locationCallback,
+                    Looper.getMainLooper()
+                )
+            } else {
+                fusedLocationProviderClient.removeLocationUpdates(locationCallback)
+            }
+        }
+    }
+
+    val locationCallback = object : LocationCallback() {
+        override fun onLocationResult(result: LocationResult) {
+            super.onLocationResult(result)
+            if (isTracking.value!!) {
+                result.locations.let { locations ->
+                    for (location in locations) {
+                        addPathPoint(location)
+                        Timber.d("New Location ${location.latitude},${location.longitude}")
+                    }
+                }
+            }
+        }
+
+    }
+
+    private fun addPathPoint(location: Location?) {
+        location?.let {
+            val pos = LatLng(location.latitude, location.longitude)
+            pathPoints.value?.apply {
+                last().add(pos)
+                pathPoints.postValue(this)
+            }
+        }
+    }
+
+    private fun addEmptyPolyLine() = pathPoints.value?.apply {
+        add(mutableListOf())
+        pathPoints.postValue(this)
+    } ?: pathPoints.postValue(mutableListOf(mutableListOf()))
+
     private fun startForegroundService() {
+        addEmptyPolyLine()
+        isTracking.postValue(true)
         val notificationManager =
             getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-        if (Build.VERSION.SDK_INT==Build.VERSION_CODES.O){
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             createNotificationChannel(notificationManager)
         }
-        val notificationBuilder=NotificationCompat.Builder(this, NOTIFICATION_CHANNEL_ID)
+        val notificationBuilder = NotificationCompat.Builder(this, NOTIFICATION_CHANNEL_ID)
             .setAutoCancel(false)
             .setOngoing(true)
             .setSmallIcon(R.drawable.ic_run)
@@ -63,17 +176,18 @@ class TrackingService : LifecycleService() {
             .setContentText("00:00:00")
             .setContentIntent(getMainActivityPendingIntent())
 
-        startForeground(NOTIFICATION_ID,notificationBuilder.build() )
+        startForeground(NOTIFICATION_ID, notificationBuilder.build())
     }
 
-    private fun getMainActivityPendingIntent()=PendingIntent.getActivity(
+    private fun getMainActivityPendingIntent() = PendingIntent.getActivity(
         this,
         0,
-        Intent(this,MainActivity::class.java).also {
-            it.action= ACTION_SHOW_TRACKING_FRAGMENT
+        Intent(this, MainActivity::class.java).also {
+            it.action = ACTION_SHOW_TRACKING_FRAGMENT
         },
-        FLAG_UPDATE_CURRENT
+        FLAG_MUTABLE
     )
+
     @RequiresApi(Build.VERSION_CODES.O)
     private fun createNotificationChannel(notificationManager: NotificationManager) {
         val channel = NotificationChannel(
--- app/src/main/java/com/example/trackapp/ui/fragment/TrackingFragment.kt
+++ app/src/main/java/com/example/trackapp/ui/fragment/TrackingFragment.kt
@@ -5,38 +5,121 @@ import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.MediatorLiveData
+import androidx.lifecycle.Observer
 import com.example.trackapp.R
+import com.example.trackapp.other.Constants.ACTION_PAUSE_SERVICE
 import com.example.trackapp.other.Constants.ACTION_START_OR_RESUME_SERVICE
+import com.example.trackapp.other.Constants.MAP_ZOOM
+import com.example.trackapp.other.Constants.POLYLINE_COLOR
+import com.example.trackapp.other.Constants.POLYLINE_WIDTH
+import com.example.trackapp.services.Polyline
 import com.example.trackapp.services.TrackingService
 import com.example.trackapp.ui.viewmodels.MainViewModel
+import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.MapView
+import com.google.android.gms.maps.model.PolylineOptions
 import com.google.android.material.button.MaterialButton
+import com.google.android.material.shape.ShapePath.PathLineOperation
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class TrackingFragment : Fragment(R.layout.fragment_tracking) {
 
+    val viewModel: MainViewModel by viewModels()
+
+    private lateinit var btnToggleRun: MaterialButton
+    private lateinit var btnFinishRun: MaterialButton
+
     private var map: GoogleMap? = null
-    private var mapView: MapView?=null
+    private var mapView: MapView? = null
+
+    private var isTracking = false
+    private var pathPoints = mutableListOf<Polyline>()
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val btnToggleRun=view.findViewById<MaterialButton>(R.id.btnToggleRun)
+        btnToggleRun = view.findViewById<MaterialButton>(R.id.btnToggleRun)
+        btnFinishRun = view.findViewById<MaterialButton>(R.id.btnFinishRun)
         mapView = view.findViewById(R.id.mapView)
         mapView?.onCreate(savedInstanceState)
-
+        btnToggleRun.setOnClickListener {
+            toggleRun()
+        }
         mapView?.getMapAsync {
             map = it
+            addAllPolylines()
         }
+        subscribeToObserver()
+    }
 
-        btnToggleRun.setOnClickListener{
+    private fun subscribeToObserver(){
+        TrackingService.isTracking.observe(viewLifecycleOwner, Observer {
+            updateTracking(it)
+        })
+
+        TrackingService.pathPoints.observe(viewLifecycleOwner, Observer {
+            pathPoints=it
+            addLatestPolyLine()
+            moveCameraToUser()
+        })
+    }
+    private fun toggleRun(){
+        if (isTracking){
+            sendCommendToService(ACTION_PAUSE_SERVICE)
+        }else{
             sendCommendToService(ACTION_START_OR_RESUME_SERVICE)
         }
     }
+    private fun updateTracking(isTracking: Boolean) {
+        this.isTracking = isTracking
+        if (!isTracking) {
+            btnToggleRun.text = "start"
+            btnFinishRun.visibility = View.VISIBLE
+        } else {
+            btnToggleRun.text = "start"
+            btnFinishRun.visibility = View.VISIBLE
+        }
+    }
 
-    private fun sendCommendToService(action:String)=
-        Intent(requireContext(),TrackingService::class.java).also {
-            it.action=action
+    private fun moveCameraToUser() {
+        if (pathPoints.isNotEmpty() && pathPoints.last().isNotEmpty()) {
+            map?.animateCamera(
+                CameraUpdateFactory.newLatLngZoom(
+                    pathPoints.last().last(),
+                    MAP_ZOOM
+                )
+            )
+        }
+    }
+
+    private fun addAllPolylines() {
+        for (polyline in pathPoints) {
+            val polylineOptions = PolylineOptions()
+                .color(POLYLINE_COLOR)
+                .width(POLYLINE_WIDTH)
+                .addAll(polyline)
+            map?.addPolyline(polylineOptions)
+        }
+    }
+
+    private fun addLatestPolyLine() {
+        if (pathPoints.isNotEmpty() && pathPoints.last().size > 1) {
+            val preLastLatLong = pathPoints.last()[pathPoints.last().size - 2]
+            val lastLatLng = pathPoints.last().last()
+            val polylineOptions = PolylineOptions()
+                .color(POLYLINE_COLOR)
+                .width(POLYLINE_WIDTH)
+                .add(preLastLatLong)
+                .add(lastLatLng)
+            map?.addPolyline(polylineOptions)
+        }
+    }
+
+    private fun sendCommendToService(action: String) =
+        Intent(requireContext(), TrackingService::class.java).also {
+            it.action = action
             requireContext().startService(it)
         }
 
@@ -54,6 +137,7 @@ class TrackingFragment : Fragment(R.layout.fragment_tracking) {
         super.onStop()
         mapView?.onStop()
     }
+
     override fun onPause() {
         super.onPause()
         mapView?.onPause()
